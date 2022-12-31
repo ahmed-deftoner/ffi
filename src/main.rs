be508ff636708ab91d24b0d5db3b6baad2624eb4
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-12-31 23:18:32 +0500

    simple function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate libc;
+
+extern {
+    fn triple_input(input: libc::c_int) -> libc::c_int;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = 4;
+    let output = unsafe { triple_input(input) };
+    println!("{} * 3 = {}", input, output);
 }
